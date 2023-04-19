b945e228e5833d2e850f557524ea8bfc35cf185a
robert casanova <robertcasanova@ufl.edu>
2023-04-19 19:52:23 -0400

    Update argumentCalculator.java

--- argumentCalculator.java
+++ argumentCalculator.java
@@ -51,9 +51,19 @@ public class argumentCalculator {
 		return product;
 	}
 
-	static void sort(int[] nums) {
-		Arrays.sort(nums);
-		System.out.println(Arrays.toString(nums));
+	static void sort(int[] arr) {
+		int n = arr.length;  
+        int temp = 0;  
+		for(int i = 0; i < n; i++) {  
+			for(int j = 1; j < (n - i); j++) {  
+				if(arr[j-1] > arr[j]) {   
+					temp = arr[j-1];  
+					arr[j-1] = arr[j];  
+					arr[j] = temp;  
+				}  
+			}  
+		}  
+		System.out.println(Arrays.toString(arr));
 	}
 
 	public static void main(String[] args) {
